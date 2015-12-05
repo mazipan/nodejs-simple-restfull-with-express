f923d878daed176cb462f16a4a5ea12c69a3f17e
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2015-12-06 06:30:38 +0700

    fixing some error in API
    update with bug-fixing and prepare for unit test

--- model/product.js
+++ model/product.js
@@ -7,8 +7,8 @@ var mongoose = require('../connection/connection');
 var Schema = mongoose.Schema;
 var Product = new Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Double , required: true },
+    description: { type: String},
+    price: { type: Number},
     modified: { type: Date, default: Date.now }
 });
 var ProductModel = mongoose.model('Product', Product);
--- routes/products.js
+++ routes/products.js
@@ -9,55 +9,72 @@ var router = express.Router();
 router.get('/', function (req, res){
     return ProductModel.find(function (err, products) {
         if (!err) {
-            return res.send(products);
+            return res.send({result : true, products : products});
         } else {
-            return console.log(err);
+            console.log(err);
+            return res.send({result : false, errorDesc : "Failed get data from DB."});
         }
     });
 });
 
 router.post('/', function (req, res){
     var product;
-    product = new ProductModel({
-        title: req.body.title,
-        description: req.body.description,
-        price: req.body.price
-    });
-    product.save(function (err) {
-        if (!err) {
-            return console.log("product has been created");
-        } else {
-            console.log(err);
-            return res.send('error when create product');
+    var errorMessage = "";
+    if(typeof req !== 'undefined'){
+        if(req.body.title === null || req.body.title === ""){
+            errorMessage = "Title product is null or empty.";
+            return res.send({result : false, errorDesc : errorMessage});
+        }else{
+            product = new ProductModel({
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price
+            });
+
+            product.save(function (err) {
+                if (!err) {
+                    console.log("product : "+product.title + " has been created ");
+                    return res.send({result : true, product : product});
+                } else {
+                    console.log(err);
+                    return res.send({result : false, errorDesc : err});
+                }
+            });
+
         }
-    });
-    return res.send(product);
+    }else{
+        errorMessage = "Request is null or empty.";
+        return res.send({result : false, errorDesc : errorMessage});
+    }
 });
 
 router.get('/:id', function (req, res){
     return ProductModel.findById(req.params.id, function (err, product) {
         if (!err) {
             console.log('get product '+ req.params.id);
-            return res.send(product);
+            return res.send({result : true, product : product});
         } else {
             console.log(err);
-            return res.send('error when get product '+ req.params.id);
+            return res.send({result : false, errorDesc : 'error when get product '+ req.params.id});
         }
     });
 });
 
 router.put('/:id', function (req, res){
     return ProductModel.findById(req.params.id, function (err, product) {
-        product.title = req.body.title;
+        if(req.body.title !== null && req.body.title !== ""){
+            product.title = req.body.title;
+        }
         product.description = req.body.description;
         product.price = req.body.price;
         return product.save(function (err) {
             if (!err) {
                 console.log("product has been updated "+ req.params.id);
+                return res.send({result : true, product : product});
             } else {
                 console.log(err);
+                return res.send({result : false, errorDesc : 'error when update product '+ req.params.id});
             }
-            return res.send(product);
         });
     });
 });
@@ -66,11 +83,11 @@ router.delete('/:id', function (req, res){
     return ProductModel.findById(req.params.id, function (err, product) {
         return product.remove(function (err) {
             if (!err) {
-                console.log("product removed");
-                return res.send('product '+ req.params.id +'has been removed');
+                console.log("product "+ req.params.id +" removed !");
+                return res.send({result : true, message : 'product '+ req.params.id +' has been removed'});
             } else {
                 console.log(err);
-                return res.send('error when remove product '+ req.params.id);
+                return res.send({result : false, errorDesc : 'error when remove product '+ req.params.id});
             }
         });
     });
